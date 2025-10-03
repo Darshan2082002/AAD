874e46e4705bf561c3b9867573ef944c582e6b82
Darshan B <87748126+Darshan2082002@users.noreply.github.com>
2025-10-03 22:37:04 +0530

    Update DIjkstra.py

--- DIjkstra.py
+++ DIjkstra.py
@@ -29,7 +29,7 @@ def dijkstra(V, edges, src):
 if __name__ == "__main__":
     V = 5
     src = 0
-    edges = [[0, 1, 4], [0, 2, 8], [1, 4, 6], [2, 3, 2], [3, 4, 10]]
+    edges = [[0, 1, 4], [0, 2, 8], [1, 4, 6], [2, 3, 2], [3, 4, 10],[4,5,6]]
 
     result = dijkstra(V, edges, src)
 
