9a7273799bb7be260b0ba686893c3d2d8c2077a3
Darshan2082002 <darshanbalu20@gmail.com>
2025-10-05 11:45:25 +0530

    Bellman ford done

--- Bellman_forward.py
+++ Bellman_forward.py
@@ -4,10 +4,10 @@ def ford(V,edge,src):
     for i in range(V):
         for edge in edges:
             u,v,wt=edge
-            if(dist[u]!=100000000 and dist[u]+wt<dist[v]):
-                if i==v-1:
+            if dist[u]!=100000000 and dist[u]+wt<dist[v]:
+                if i==V-1:
                     return [-1]
-                dist[v]+dist[u]+wt                
+                dist[v]=dist[u]+wt                
     return dist
 
 if __name__=="__main__":
