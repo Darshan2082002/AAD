788f9ddd3f1cb065a0af729a6486b448982dc11f
Darshan2082002 <darshanbalu20@gmail.com>
2025-10-05 11:02:41 +0530

    deploy

--- Warshall_Flyod.py
+++ Warshall_Flyod.py
@@ -6,3 +6,19 @@ def flyod(arr):
                 if (arr[i][k]!=100000000 and arr[j][k]!=100000000):
                     arr[i][j]=min(arr[i][j],arr[i][k]+arr[j][k])
                     
+if __name__ == "__main__":
+    
+    INF = 100000000
+    dist = [
+        [0, 4, INF, 5, INF],
+        [INF, 0, 1, INF, 6],
+        [2, INF, 0, 3, INF],
+        [INF, INF, 1, 0, 2],
+        [1, INF, INF, 4, 0]
+    ]
+    
+    floydWarshall(dist)
+    for i in range(len(dist)):
+        for j in range(len(dist)):
+            print(dist[i][j], end=" ")
+        print()
