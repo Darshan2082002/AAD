784fa455a5ef577a320c919eb70f7f16d995d8b8
Darshan2082002 <darshanbalu20@gmail.com>
2025-10-05 11:42:51 +0530

    deploy

--- Bellman_forward.py
+++ Bellman_forward.py
@@ -8,3 +8,12 @@ def ford(V,edge,src):
                 if i==v-1:
                     return [-1]
                 dist[v]+dist[u]+wt                
+    return dist
+
+if __name__=="__main__":
+     V = 5
+    edges = [[1, 3, 2], [4, 3, -1], [2, 4, 1], [1, 2, 1], [0, 1, 5]]
+
+    src = 0
+    ans = ford(V, edges, src)
+    print(' '.join(map(str, ans)))
